2531982126bd09c00d52f746dee8cadba3e2350a
Biraru <muhammadbilalalisyed@gmail.com>
2024-12-27 23:58:43 +0500

    Changed Versions

--- twrpdtgen_v2/__init__.py
+++ twrpdtgen_v2/__init__.py
@@ -11,7 +11,7 @@ from pathlib import Path
 
 from twrpdtgen_v2.proprietary_files.section import register_sections
 
-__version__ = "2.0.5"
+__version__ = "2.0.0"
 
 module_path = Path(__file__).parent
 sections_path = module_path / "proprietary_files" / "sections"
